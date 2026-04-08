709e0ca91e045cfd53bd66a3857de1138afc359c
Ddrewy <deandrew.y@gmail.com>
2026-04-08 13:19:04 +1000

    update: update the navbar and landing page

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,32 +1,28 @@
-"use client";
-
 import Link from 'next/link';
-import { useState } from 'react';
 
 export default function NavBar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleOpen = () => {
-        setIsOpen(!isOpen);
-    }
 
     return (
         <div className='sticky top-0'>
             <div className='flex flex-row bg-fucoraCards pt-1.5 pb-1.5 shadow-xl shadow-fucoraButton/18 justify-between'>
-                <Link href="/" className='pt-1.5 pl-1.5'>
-                    <img src="/favicon.png" alt='Logo' className='w-10 h-10'/>
-                </Link>
-                <div className='pr-10'>
-                    <button onClick={handleOpen}>
-                        {isOpen ? <img className="w-10 h-10" src="./closeHamburg.svg"/> : <img className="w-10 h-10" src="./hamburg.svg"/>}
-                    </button>
-                    
+                <div className='flex flex-row pl-1'>
+                    <Link href="/" className='pt-1.5 pl-1.5'>
+                        <img src="/favicon.png" alt='Logo' className='w-10 h-10'/>
+                    </Link>
+
+                    <Link href="/about" className='pl-2.5 pt-3 text-fucoraText text-xl'>About</Link>
+                    <Link href="/projects" className='pl-2.5 pt-3 text-fucoraText text-xl'>Projects</Link>
                 </div>
-            </div>
-            <div style={{visibility: isOpen ? 'visible' : 'hidden'}} className='flex flex-col absolute right-0 pr-10 h-screen w-40 items-center text-xl pt-5 bg-fucoraCards'>
-                <Link href="/about" className='pl-5 pt-3 text-fucoraText'>About</Link>
-                <Link href="/projects" className='pl-5 pt-3 text-fucoraText'>Projects</Link>
-                <Link href='/contact' className='pl-5 pt-3 text-fucoraText'>Contact</Link>
+                
+                <div className='flex flex-row mt-3 mr-2'>
+                    <Link href='/' className='mr-2'>
+                        <img className="w-7 h-7" src='./Instagram_Glyph_White.svg' alt='Instagram'/>
+                    </Link>
+                    <Link href='https://github.com/fucora-studio' className='ml-3 mr-5'>
+                        <img className='w-7 h-7' src='./GitHub_Invertocat_White.svg' alt='GitHub'/>
+                    </Link>
+                </div>
+
             </div>
         </div>
     )
